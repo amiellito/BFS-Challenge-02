dac987f1a69fa1a59ab381adfef49f427eadf5fa
amiellito <amiel.lapuebla@gmail.com>
2020-07-15 01:50:41 -0700

    Implemented POST and voting

--- app.js
+++ app.js
@@ -1,69 +1,160 @@
 const api_key = "$api_key=50d57945-64d7-4f99-9bb3-46d5026782fb"
-const url = 'https://api.thecatapi.com/v1/images/search?mime_types=jpg&format=landscape' + api_key;
+const url = 'https://api.thecatapi.com/v1/images/search?mime_types=jpg&format=landscape';
 const quotes ='https://api.kanye.rest/'
 const title = './title.json'
 const brit = './british.json'
 
 
+fetch(url)
+  .then(results => results.json())
+  .then(data => 
+    {
+    document.getElementById("app").innerHTML=`<img  class="img-container" src="${data[0].url}">`
 
-async function getKitty(){
-  const response = await fetch(url);
-  const data = await response.json();
+      function yes(){
+        vote = 1;
+      }
 
+      function no(){
+        vote = 0;
+      }
 
-  document.getElementById("app").innerHTML=`
+      document.getElementById("yes").onclick = yes();
+      document.getElementById("no").onclick = no();
     
-  <img  class="img-container" src="${data[0].url}">
+      //Create Object
 
-  `
-
-}
-
-async function getQuote(){
-  const response = await fetch(quotes);
-  const qData = await response.json();
+      const vData = { 
+        "image_id": data[0].id,
+        "sub_id": "guest_user",
+        "value": vote
+       };
 
 
-  document.getElementById("quote").innerHTML=`
-    
-  <p>${qData.quote}</p>
-
-  `
-
-}
-
-async function getTitle(){
-  const response = await fetch(title);
-  const tData = await response.json();
-  const titleRandom = Math.floor(Math.random()*tData.length);
-
-
-  document.getElementById("title").innerHTML=`
-    
-  <h2>${tData[titleRandom].title}</h2>
-
-  `
-
-}
-
-async function getEng(){
-  const response = await fetch(brit);
-  const bData = await response.json();
-  const engRandom = Math.floor(Math.random()*bData.length);
+      fetch('https://api.thecatapi.com/v1/votes?', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': '50d57945-64d7-4f99-9bb3-46d5026782fb'
+        },
+        body: JSON.stringify(vData),
+      })
+        .then(response => response.json())
+        .then(vData => {
+          console.log('Success:', vData);
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
 
 
-  document.getElementById("eng").innerHTML=`
-    
-  ${bData[engRandom].english} !
-
-  `
-
-}
+    }
+    )
 
 
 
-getKitty()
-getQuote()
-getTitle()
-getEng()
+
+fetch(quotes)
+  .then(results => results.json())
+  .then(qData => 
+  
+  document.getElementById("quote").innerHTML=`<p>${qData.quote}</p>`
+  
+  )
+
+
+fetch(title)
+  .then(results => results.json())
+  .then(tData => {
+    const titleRandom = Math.floor(Math.random()*tData.length);
+  
+    document.getElementById("title").innerHTML=`<h2>${tData[titleRandom].title}</h2>`
+
+  }
+  
+  )
+
+fetch(brit)
+  .then(results => results.json())
+  .then(bData => {
+    const engRandom = Math.floor(Math.random()*bData.length);
+  
+    document.getElementById("eng").innerHTML=`${bData[engRandom].english} !`
+
+  }
+  
+  )
+
+
+////////////////////////////////////
+
+//VOTING: I NEED TO CREATE THIS:
+
+// {
+//   "image_id": "asf2",
+//   "sub_id": "my-user-1234",
+//   "value": 1
+// }
+
+// AND SEND IT HERE: https://api.thecatapi.com/v1/votes?
+
+//HEADER: x-api-key: 50d57945-64d7-4f99-9bb3-46d5026782fb
+//CONTENT-TYPE: application/json
+
+
+///////////////////////////////// 
+
+//**** CREATE AN OBJECT ******//
+
+// Create vote object
+
+// const voteObj = new Object();
+
+// voteObj.image_id = data[0].id;
+// voteObj.sub_id = "guest_user";
+// voteObj.value = voteValue;
+
+
+// console.log(voteObj);
+
+// console.log("vote object: " + JSON.stringify(voteObj));
+// console.log(url);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
